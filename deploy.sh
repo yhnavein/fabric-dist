5229256517deafea997fa098b6554c4a4cf6e09c
Piotrek Dąbrowski <floubadour@gmail.com>
2018-03-21 01:09:29 +0100

    fix: fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
 
-npm install
+yarn
 
 cp node_modules/fabric/dist/fabric.js ./fabric.js
 
-# npm publish
+VERSION=$(jq -r '.version' package.json)
+
+echo "Version to publish: $VERSION"
+
+npm publish
+
+git add package.json
+git commit -m "chore: published version $VERSION"
